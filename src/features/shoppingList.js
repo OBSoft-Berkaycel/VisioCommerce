7564a9a70dc5480bb5dbc566165625020aa8ca75
OBSoft-Berkaycel <berkaycel@gmail.com>
2024-10-25 17:59:52 +0300

    unnecessary comment lines were removed

--- src/features/shoppingList.js
+++ src/features/shoppingList.js
@@ -1,21 +1,21 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/v1/shopping-list'; // API URL'ini buraya ekleyin
+const API_URL = 'http://localhost:8000/v1/shopping-list'; 
 
 export const getList = createAsyncThunk('shoppingList/getList', async () => {
   const response = await axios.get(`${API_URL}/listAll`);
-  return response.data; // API'den gelen yanıtı döndür
+  return response.data;
 });
 
 export const addItem = createAsyncThunk('shoppingList/addItem', async (item) => {
   const response = await axios.post(`${API_URL}/addNewItem`, item);
-  return response.data; // API'den gelen yanıtı döndür
+  return response.data;
 });
 
 export const togglePurchased = createAsyncThunk('shoppingList/togglePurchased', async (id) => {
   const response = await axios.put(`${API_URL}/update`, { id });
-  return response.data; // API'den gelen yanıtı döndür
+  return response.data;
 });
 
 const shoppingListSlice = createSlice({
@@ -23,7 +23,7 @@ const shoppingListSlice = createSlice({
   initialState: {
     list: [],
     selectedList: [],
-    status: 'idle', // Async durumunu takip etmek için
+    status: 'idle',
     error: null,
   },
   reducers: {
@@ -41,15 +41,15 @@ const shoppingListSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(getList.fulfilled, (state, action) => {
-        state.list = action.payload; // API'den alınan veriyi state'e ekle
+        state.list = action.payload;
       })
       .addCase(addItem.fulfilled, (state, action) => {
-        state.list.push(action.payload); // Yeni ürünü listeye ekle
+        state.list.push(action.payload);
       })
       .addCase(togglePurchased.fulfilled, (state, action) => {
         const item = state.list.find(item => item.id === action.payload.id);
         if (item) {
-          item.purchased = !item.purchased; // Satın alındı durumunu değiştir
+          item.purchased = !item.purchased;
         }
       });
   },
